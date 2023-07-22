964ff3daae88af0a53248ec39abb441871b0c0f1
Raihan Evan <raihan.evan@gmail.com>
2023-07-22 23:16:23 +0600

    item

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,42 +1,81 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_todo1/constants/colors.dart';
-void main() => runApp(
-    MaterialApp(home: Home()));
+import 'package:flutter_todo1/widgets/todo_item.dart';
 
-class Home extends StatelessWidget{
-  const Home({Key? key}) : super(key:key);
+void main() => runApp(MaterialApp(home: Home()));
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: tdBGColor,
       appBar: _buildAppBar(),
       body: Container(
-        child: Text('Home Screen'),
+        padding: EdgeInsets.symmetric(horizontal: 18, vertical: 20),
+        child: Column(
+          children: [
+            searchBox(),
+            Expanded(
+                child: ListView(
+              children: [
+                Container(
+                  margin: EdgeInsets.only(top: 50,bottom: 20),
+                  child: Text('All Todo Lists', style: TextStyle(fontSize: 30,fontWeight: FontWeight.w400),),
+                ),
+                ToDoItem(),
+              ],
+            )
+    )
+          ],
+        ),
       ),
     );
   }
-  AppBar _buildAppBar(){
+
+  AppBar _buildAppBar() {
     return AppBar(
-  backgroundColor: tdBGColor,
-  elevation: 22,
-  title: Row(
-  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-  children: [
-  Icon(
-  Icons.menu,
-  color: tdBlack,
-  size: 30,
-  ),
-  Container(
-  height: 40,
-  width: 40,
-  child: ClipRRect(
-  borderRadius: BorderRadius.circular(35),
-  child: Image.asset('assets/images/evan2-compressed.jpg'),
-  ),
-  )
-  ])
-    );
+        backgroundColor: tdBGColor,
+        elevation: 2,
+        title:
+            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+          Icon(
+            Icons.menu,
+            color: tdBlack,
+            size: 30,
+          ),
+          Container(
+            height: 40,
+            width: 40,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(35),
+              child: Image.asset('assets/images/evan2-compressed.jpg'),
+            ),
+          )
+        ]));
+  }
+   Widget searchBox() {
+    return Container(
+          padding: EdgeInsets.symmetric(horizontal: 15),
+          decoration: BoxDecoration(
+              color: Colors.greenAccent,
+              borderRadius: BorderRadius.circular(20)),
+          child: TextField(
+            decoration: InputDecoration(
+              contentPadding: EdgeInsets.all(0),
+              prefixIcon: Icon(
+                Icons.search,
+                color: tdBlack,
+                size: 22,
+              ),
+              prefixIconConstraints:
+              BoxConstraints(maxHeight: 20, minWidth: 25),
+              border: InputBorder.none,
+              hintText: 'Search Here',
+              hintStyle: TextStyle(color: tdGrey),
+            ),
+          ),
+        );
   }
 }
